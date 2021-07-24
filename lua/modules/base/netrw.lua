5bb3496f71a104b2be63a58b1b55aec53c99b4f4
macabre <crosley40@gmail.com>
2021-07-24 18:31:01 +0700

    add directory highlights

--- lua/modules/base/netrw.lua
+++ lua/modules/base/netrw.lua
@@ -3,4 +3,7 @@ vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
 vim.g.netrw_liststyle = 3
 vim.g.netrw_browser_split = 4
+
+-- Directory Highlights
+vim.cmd('au ColorScheme * hi Directory guifg=#CCCCCC')
 ---------------------------------------------------
