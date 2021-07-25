a8d59dc20e9b2dc495f6a47f16b2e9458988ac89
macabre <crosley40@gmail.com>
2021-07-25 16:27:11 +0700

    add modules (accidentaly deleted)

---
+++ lua/modules/base/netrw.lua
@@ -0,0 +1,9 @@
+-- [ Netrw ] --------------------------------------
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+vim.g.netrw_liststyle = 3
+vim.g.netrw_browser_split = 4
+
+-- Directory Highlights
+vim.cmd('au ColorScheme * hi Directory guifg=#CCCCCC')
+---------------------------------------------------
